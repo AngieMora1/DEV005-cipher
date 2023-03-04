ed59e1c1c16b58ed536547b064b0cf37224b7556
AngieMora1 <angiemorarosas@gmail.com>
2023-03-03 21:57:38 -0500

    Sube proyecto Cipher al repositorio

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,41 @@
 const cipher = {
-  // ...
+  encode: function (desplazamiento, mensajeUsuario) {
+    let menUsuario = "";
+    mensajeUsuario = mensajeUsuario.toUpperCase();
+    desplazamiento = parseInt(desplazamiento);
+    if (mensajeUsuario !== "" && desplazamiento > 0) {
+      for (let i = 0; i < mensajeUsuario.length; i++) {
+        const codigoAscii = mensajeUsuario.charCodeAt(i);
+        if (codigoAscii >= 65 && codigoAscii <= 90) {
+          const formula = ((codigoAscii + desplazamiento - 65) % 26) + 65;
+          const letraCifrado = String.fromCharCode(formula);
+          menUsuario+=letraCifrado;
+        }else{
+          menUsuario += " ";
+        }
+      }
+    }
+    return menUsuario;
+  },
+
+  decode: function (desplazamiento, mensajeUsuario) {
+    let menUsuario = "";
+    mensajeUsuario = mensajeUsuario.toUpperCase();
+    desplazamiento = parseInt(desplazamiento);
+    if (mensajeUsuario !== "" && desplazamiento > 0) {
+      for (let i = 0; i < mensajeUsuario.length; i++) {
+        const codigoAscii = mensajeUsuario.charCodeAt(i);
+        if (codigoAscii >= 65 && codigoAscii <= 90) {
+          const formula =((codigoAscii - desplazamiento + 65) % 26) + 65;
+          const letraDescifradO = String.fromCharCode(formula);
+          menUsuario += letraDescifradO;
+        }else{
+          menUsuario += " ";
+        }
+      }
+    }
+    return menUsuario;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
+//Elementos del DOM 
+const resultadoMensaje = document.getElementById("resultado-mensaje");
+const btnCifrar = document.getElementById("btn-cifrar");
+const btnDescifrar = document.getElementById("btn-descifrar");
 
-console.log(cipher);
+// Hasta aqui solo se ha declarado y asignado la variable
+//Funciones que interactuan con el DOM
+function mostrarCifrado() {
+  const mensajeUsuario = document.getElementById("mensaje-usuario").value;
+  const desplazamiento = document.getElementById("desplazamiento-usuario").value;
+
+  resultadoMensaje.value = cipher.encode(desplazamiento, mensajeUsuario);
+}
+function mostrarDescifrado() {
+  const mensajeUsuario = document.getElementById("mensaje-usuario").value;
+  const desplazamiento = document.getElementById(
+    "desplazamiento-usuario"
+  ).value;
+
+  resultadoMensaje.value = cipher.decode(desplazamiento, mensajeUsuario);
+}
+//Eventos del DOM(EJ.CLICK)
+btnCifrar.addEventListener("click", mostrarCifrado);
+btnDescifrar.addEventListener("click", mostrarDescifrado);
+
